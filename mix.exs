61541122fad85e0bcdf77f225d8cb7044e6a9f5c
Isaac Yonemoto <isaac@rstor.io>
2019-07-01 09:51:44 -0700

    feat:  integration of ex_doc and excoveralls

--- mix.exs
+++ mix.exs
@@ -7,7 +7,9 @@ defmodule ExDhcp.MixProject do
       version: "0.1.0",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [coveralls: :test, "coveralls.detail": :test, "coveralls.post": :test, "coveralls.html": :test]
     ]
   end
 
@@ -22,8 +24,11 @@ defmodule ExDhcp.MixProject do
   defp deps do
     [
       {:elixir_uuid, "~> 1.2", only: [:test, :dev]},
-      {:credo, "~> 1.1", only: [:test, :dev]},
-      {:dialyxir, "~> 0.5.1", only: [:test, :dev]}
+      {:credo, "~> 1.1", only: [:test, :dev], runtime: false},
+      {:dialyxir, "~> 0.5.1", only: :dev, runtime: false},
+      {:licensir, "~> 0.4.2", only: :dev, runtime: false},
+      {:ex_doc, "~> 0.20.2", only: :dev, runtime: false},
+      {:excoveralls, "~> 0.11.1", only: :test}
     ]
   end
 end
